5ff12ab965cd55b3f59c30a21c40e4401b592226
LesterL <lester_lecong@yahoo.com>
2021-08-24 10:30:54 +0800

    autocommit 20210824.103054

--- autocommit.bat
+++ autocommit.bat
@@ -1,4 +1,4 @@
 echo > .gitignore
 git add --all
 git commit -m "autocommit %date:~-4%%date:~3,2%%date:~0,2%.%time:~0,2%%time:~3,2%%time:~6,2%"
-git push https://lesterlecong:S3agat3@github.com/lesterlecong/lesterlecong.github.io.git
+git push https://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@github.com/lesterlecong/lesterlecong.github.io.git
